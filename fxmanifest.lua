fa6ddd9c4707ecd2216b305b4743a812cf064561
WasabiRobby <79250053+WasabiRobby@users.noreply.github.com>
2022-06-10 14:25:33 -0700

    chore: updated version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,11 @@
 -----------------For support, scripts, and more----------------
------------------https://discord.gg/XJFNyMy3Bv-----------------
+----------------- https://discord.gg/XJFNyMy3Bv ---------------
 ---------------------------------------------------------------
 fx_version 'cerulean'
 game 'gta5'
 description 'wasabi_tireslash - A target based tire slashing script.'
+author 'wasabirobby#5110'
+version '1.0.2'
 
 client_scripts {
     'client/**.lua'
