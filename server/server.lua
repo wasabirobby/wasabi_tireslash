d84f3f5ddccd6fd8c521a0bd86fab221d1c46fa5
wasabirobby <79250053+wasabirobby@users.noreply.github.com>
2022-09-30 16:29:05 -0700

    chore: update support link

--- server/server.lua
+++ server/server.lua
@@ -1,5 +1,5 @@
 -----------------For support, scripts, and more----------------
------------------ https://discord.gg/XJFNyMy3Bv ---------------
+--------------- https://discord.gg/wasabiscripts  -------------
 ---------------------------------------------------------------
 RegisterServerEvent('wasabi_tireslash:sync')
 AddEventHandler('wasabi_tireslash:sync', function(id, tireIndex)
