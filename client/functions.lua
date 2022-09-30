7b4d3488e2ec37117a3d873901aa583979696935
wasabirobby <79250053+wasabirobby@users.noreply.github.com>
2022-09-30 16:28:51 -0700

    chore: update support link

--- client/functions.lua
+++ client/functions.lua
@@ -1,5 +1,5 @@
 -----------------For support, scripts, and more----------------
------------------https://discord.gg/XJFNyMy3Bv-----------------
+--------------- https://discord.gg/wasabiscripts  -------------
 ---------------------------------------------------------------
 loadDict = function(dict)
     while not HasAnimDictLoaded(dict) do Wait(0) RequestAnimDict(dict) end
